0a93748f06992d82d683549703cb77c51dab2d84
Rajan kumar <krcpr007@gmail.com>
2022-06-22 13:50:36 +0530

    setuser done

--- src/utils/reducer.js
+++ src/utils/reducer.js
@@ -1,18 +1,35 @@
 import { reducerCases } from "./Constants";
 export const initialState = {
-    token: null
+    token: null,
+    userInfo: null,
+    playlists: [],
+    currentPlaying: null,
+    playerState: false,
+    selectedPlaylist: null,
+    selectedPlaylistId: "37i9dQZF1E37jO8SiMT0yN",
 }
 
 const reducer = (state, action) => {
     switch (action.type) {
-        case reducerCases.SET_TOKEN:{
-            return{
-                ...state, 
-                token:action.token,
+        case reducerCases.SET_TOKEN: {
+            return {
+                ...state,
+                token: action.token,
             };
         }
+        case reducerCases.SET_PLAYLISTS:
+            return {
+                ...state,
+                playlists: action.playlists,
+            };
+        case reducerCases.SET_USER:
+            return {
+                ...state,
+                userInfo: action.userInfo,
+            };
         default:
             return state;
+
     }
 }
 export default reducer; 
