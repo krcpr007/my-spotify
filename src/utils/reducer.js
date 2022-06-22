62adcb921c49c14fc6d4f070397eb97f30785f2f
Rajan kumar <krcpr007@gmail.com>
2022-06-22 17:36:13 +0530

    current track not working now need to solve

--- src/utils/reducer.js
+++ src/utils/reducer.js
@@ -27,6 +27,26 @@ const reducer = (state, action) => {
                 ...state,
                 userInfo: action.userInfo,
             };
+        case reducerCases.SET_PLAYLIST:
+            return {
+                ...state,
+                selectedPlaylist: action.selectedPlaylist,
+            };
+        case reducerCases.SET_PLAYING:
+            return {
+                ...state,
+                currentPlaying: action.currentPlaying,
+            };
+        case reducerCases.SET_PLAYER_STATE:
+            return {
+                ...state,
+                playerState: action.playerState,
+            };
+        case reducerCases.SET_PLAYLIST_ID:
+            return {
+                ...state,
+                selectedPlaylistId: action.selectedPlaylistId,
+            };
         default:
             return state;
 
