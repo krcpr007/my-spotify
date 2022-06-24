5beedc8d7f238d6299933ea50c6f60fb92dfa972
Rajan kumar <krcpr007@gmail.com>
2022-06-24 10:57:17 +0530

    pushing redirectUrl

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,9 +3,13 @@ import styled from 'styled-components';
 function Login() {
     const handleClick = () => {
         const clientId= "cbb279f49e1843dfa4ae223a53a0e8fe"; 
-        const redirectUrl = "http://localhost:3000/"
+        // const redirectUrl = "http://localhost:3000/"
+        const redirectUrl ="https://spotify-krcpr007.vercel.app/"
         const apiUrl = "https://accounts.spotify.com/authorize"; 
         const scope = [
+            "streaming",
+            "user-library-read",
+            "user-library-modify",
             "user-read-private",
             "user-read-email",
             "user-modify-playback-state",
@@ -20,7 +24,7 @@ function Login() {
         <Container>
             <img src="/Spotify_Logo_RGB_Green.png" alt="spotify-logo" />
             <button onClick={handleClick}>Connect Spotify</button>
-            <span>Made by Rajan kumar</span>
+            <span>Made by <a href="https://github.com/krcpr007" target="_blank" rel="noreferrer" >Rajan kumar</a> </span>
         </Container>
     )
 }
@@ -43,15 +47,21 @@ const Container = styled.div`
     padding: 1rem 5rem;
     border-radius: 5rem;
     background-color: #1db954;
-    color: #black;
+    color: black;
     border: none;
-    font-size: 1.4rem;
+    font-size: 1rem;
     cursor: pointer;
+      
     }
+  
     span{
         color:white;
         font:bold;
     }
+  a{
+    color: #1db954;
+    text-underline: none !important;
+  }
 `;
 
 export default Login
