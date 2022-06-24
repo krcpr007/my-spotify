312eb2b2a8afc775b1853c0553e8874e8526f120
Rajan kumar <krcpr007@gmail.com>
2022-06-24 11:01:35 +0530

    components removed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,10 @@
 import React from "react";
 import styled from "styled-components";
-import CurrentTrack from "./CurrentTrack";
-
-import PlayerControls from "./PlayerControls";
-import Volume from "./Volume";
+import PlayBack from "./PlayBack";
 export default function Footer() {
   return (
     <Container>
-      <CurrentTrack />
-      <PlayerControls />
-      <Volume />
+      <PlayBack/>
     </Container>
   );
 }
@@ -19,8 +14,8 @@ const Container = styled.div`
   width: 100%;
   background-color: #181818;
   border-top: 1px solid #282828;
-  display: grid;
-  grid-template-columns: 1fr 2fr 1fr;
+  // display: grid;
+  // grid-template-columns: 1fr 2fr 1fr;
   align-items: center;
   justify-content: center;
   padding: 0 1rem;
