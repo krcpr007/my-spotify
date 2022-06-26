946f472364090431be9927cf1ee6068dc0ff51c2
Rajan kumar <krcpr007@gmail.com>
2022-06-26 18:04:05 +0530

    padding and grid removed finaly

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,9 +14,6 @@ const Container = styled.div`
   width: 100%;
   background-color: #181818;
   border-top: 1px solid #282828;
-  // display: grid;
-  // grid-template-columns: 1fr 2fr 1fr;
   align-items: center;
   justify-content: center;
-  // padding: 0 1rem;
 `;
